869eb9ca7548e23e8928bd7aab99a14fe6aa92c2
larissa557197 <larissampmuniz@gmail.com>
2025-09-11 19:51:32 -0300

    feat(paciente): adicionar migration Flyway para criação da tabela Paciente

---
+++ src/main/resources/db/migration/V1__create_table_paciente.sql
@@ -0,0 +1,7 @@
+CREATE TABLE paciente (
+    id BIGSERIAL PRIMARY KEY -- chave primária auto-incremento,
+    nome VARCHAR(120) NOT NULL,
+    data_nascimento DATE --pode ser nulo, mas validado na aplicação com @Past,
+    email VARCHAR(100) NOT NULL UNIQUE -- para não ser duplicado,
+    telefone VARCHAR(40)
+)
