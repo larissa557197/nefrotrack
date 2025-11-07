192467f2c9d02bc4929421d0b239f98c946fabb4
larissa557197 <larissampmuniz@gmail.com>
2025-11-06 23:04:16 -0300

    refactor(exame): ExameRenalController to improve separation of concerns
    - Extracted business logic to new ExameRenalService
    - Simplified controller to focus on web layer responsibilities
    - Centralized data access and patient/exam type retrieval in service
    - Improved readability and maintainability of exam module

--- src/main/java/br/com/fiap/nefrotrack/exame/ExameRenalRepository.java
+++ src/main/java/br/com/fiap/nefrotrack/exame/ExameRenalRepository.java
@@ -1,12 +1,11 @@
 package br.com.fiap.nefrotrack.exame;
 
 import br.com.fiap.nefrotrack.paciente.Paciente;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 
 import java.util.List;
 
-public interface ExameRenalRepository extends CrudRepository<ExameRenal, Long> {
-
+public interface ExameRenalRepository extends JpaRepository<ExameRenal, Long> {
     List<ExameRenal> findByPacienteOrderByDataDesc(Paciente paciente);
-
 }
